a78811875731776c9f71aca6bfccf5ae18edf6ee
Carlos Ruiz <mastermajora@protonmail.com>
2024-11-02 09:41:17 -0600

    estructura basico correctamente funcionado

--- ISNP001724_ISNP010424_ISNP142324_Bloque3/ISNP001724_ISNP010424_ISNP142324_Bloque3/obj/Debug/net8.0/ISNP001724_ISNP010424_ISNP142324_Bloque3.AssemblyInfo.cs
+++ ISNP001724_ISNP010424_ISNP142324_Bloque3/ISNP001724_ISNP010424_ISNP142324_Bloque3/obj/Debug/net8.0/ISNP001724_ISNP010424_ISNP142324_Bloque3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ISNP001724_ISNP010424_ISNP142324_Bloque3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+306fcdc1edec843bea0fade480ea626e40c011cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ISNP001724_ISNP010424_ISNP142324_Bloque3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ISNP001724_ISNP010424_ISNP142324_Bloque3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
